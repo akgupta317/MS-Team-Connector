5b15efed0b91b202c35e3301f2d8bccdcc32b358
akgupta317 <32034513+akgupta317@users.noreply.github.com>
2021-04-14 00:22:21 +0530

    Update configConstant.ts

--- MSTeamConnectorApp_Backup/src/constant/configConstant.ts
+++ MSTeamConnectorApp_Backup/src/constant/configConstant.ts
@@ -1,5 +1,5 @@
 export  class  ConfigConstant {
-    static get Login_URL() { return "tst-ben.smart-gdpr.eu"; }
-    static get BASE_URL() { return "o365.smartglobal.com"; }  
+    static get Login_URL() { return "{domain name}"; }
+    static get BASE_URL() { return "o365.domain name"; }  
     static get SAVE_Mapping_Data() { return "/api/msteams/channel"; }    
 }
